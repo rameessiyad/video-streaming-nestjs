c4935be253ed7a88269f837056c8e6f194916312
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-14 13:24:32 +0530

    signup and signin services added

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,41 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { User, UserDocument } from 'src/model/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async signup(user: User): Promise<User> {
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(user.password, salt);
+    const reqBody = {
+      fullname: user.fullname,
+      email: user.email,
+      password: hash,
+    };
+
+    const newUser = new this.userModel(reqBody);
+    return newUser.save();
+  }
+
+  async signin(user: User, jwt: JwtService): Promise<any> {
+    const foundUser = await this.userModel
+      .findOne({ email: user.email })
+      .exec();
+    if (foundUser) {
+      const { password } = foundUser;
+      if (bcrypt.compare(user.password, password)) {
+        const payload = { email: user.email };
+        return {
+          token: jwt.sign(payload),
+        };
+      }
+      return new HttpException('Invalid credentails', HttpStatus.UNAUTHORIZED);
+    }
+    return new HttpException('Invalid credentails', HttpStatus.UNAUTHORIZED);
+  }
+}
