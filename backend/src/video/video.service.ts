6431b2bff52bcb67d79c8c319c28e2dacf6945d7
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-14 15:17:56 +0530

    video create and video get services created

--- backend/src/video/video.service.ts
+++ backend/src/video/video.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Video, VideoDocument } from 'src/model/video.schema';
 
 @Injectable()
-export class VideoService {}
+export class VideoService {
+  constructor(
+    @InjectModel(Video.name) private VideoModel: Model<VideoDocument>,
+  ) {}
+
+  async createVideo(video: object): Promise<Video> {
+    const newVideo = new this.VideoModel(video);
+    return newVideo.save();
+  }
+
+  async readVideo(id): Promise<any> {
+    if (id.id) {
+      return this.VideoModel.findOne({ _id: id.id })
+        .populate('cratedBy')
+        .exec();
+    }
+    return this.VideoModel.find().populate('cratedBy').exec();
+  }
+}
