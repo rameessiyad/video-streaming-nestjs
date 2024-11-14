315b8ca6533517ab0a66f5737e7f20ab21205ca4
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-14 12:54:21 +0530

    user module created

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/video-stream')],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/video-stream'),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
