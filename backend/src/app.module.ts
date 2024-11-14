4515552a535bbf3e24d35f640ce5164563b1e172
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-14 13:08:45 +0530

    imported jwt in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,11 +3,17 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { secret } from './utils/constants';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/video-stream'),
     UserModule,
+    JwtModule.register({
+      secret,
+      signOptions: { expiresIn: '2h' },
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
