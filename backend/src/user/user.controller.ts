38ffaeccbb59f0d781bd9aab5ad49247f7bd0e68
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-14 13:03:53 +0530

    user controller created

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { User } from 'src/model/user.schema';
+import { UserService } from './user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(
+    private readonly userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  @Post('/signup')
+  async Signup(@Res() response, @Body() user: User) {
+    const newUser = await this.userService.signup(user);
+    return response.status(HttpStatus.CREATED).json({ newUser });
+  }
+
+  @Post('/signin')
+  async Signin(@Res() response, @Body() user: User) {
+    const token = await this.userService.signin(user, this.jwtService);
+    return response.status(HttpStatus.OK).json(token);
+  }
+}
