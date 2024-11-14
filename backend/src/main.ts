500b0bc0aaffd54aa8e1edc578afe3ab41dab2e1
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-14 12:37:35 +0530

    database connected

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  const PORT = process.env.PORT ?? 5000;
+  await app
+    .listen(PORT)
+    .then(() => console.log(`Server running on port ${PORT}`));
 }
 bootstrap();
