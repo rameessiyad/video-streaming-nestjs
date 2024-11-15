3a7d846a9ab101d0808906502e2c618266cc6eeb
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-15 11:05:16 +0530

    jwt module added in user module

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -3,10 +3,16 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from 'src/model/user.schema';
+import { JwtModule } from '@nestjs/jwt';
+import { secret } from 'src/utils/constants';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule.register({
+      secret: secret,
+      signOptions: { expiresIn: '2h' },
+    }),
   ],
   providers: [UserService],
   controllers: [UserController],
