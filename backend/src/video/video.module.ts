f60dd2b323f753c5ee7b51c712c8789d9f4bde17
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-15 11:11:11 +0530

    multer module added in video module

--- backend/src/video/video.module.ts
+++ backend/src/video/video.module.ts
@@ -3,10 +3,21 @@ import { VideoService } from './video.service';
 import { VideoController } from './video.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Video, VideoSchema } from 'src/model/video.schema';
-
+import { MulterModule } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 as uuidv4 } from 'uuid';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]),
+    MulterModule.register({
+      storage: diskStorage({
+        destination: './public',
+        filename: (req, file, cb) => {
+          const ext = file.mimetype.split('/')[1];
+          cb(null, `${uuidv4()}.${ext}`);
+        },
+      }),
+    }),
   ],
   providers: [VideoService],
   controllers: [VideoController],
