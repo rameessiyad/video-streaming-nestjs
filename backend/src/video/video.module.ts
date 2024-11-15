ebe4ed1573781bc3294d819c457a94f0b2167fd8
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-15 11:00:24 +0530

    video module error fixed

--- backend/src/video/video.module.ts
+++ backend/src/video/video.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { VideoService } from './video.service';
 import { VideoController } from './video.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Video, VideoSchema } from 'src/model/video.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Video.name, schema: VideoSchema }]),
+  ],
   providers: [VideoService],
-  controllers: [VideoController]
+  controllers: [VideoController],
 })
 export class VideoModule {}
