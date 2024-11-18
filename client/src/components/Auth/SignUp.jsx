bd00dc9a3b906b6b390bbc969e99dcb24b2fb5c9
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-18 16:43:28 +0530

    signup ui completed

--- client/src/components/Auth/SignUp.jsx
+++ client/src/components/Auth/SignUp.jsx
@@ -1,5 +1,103 @@
+import {
+  Avatar,
+  Box,
+  Container,
+  CssBaseline,
+  TextField,
+  Typography,
+  Grid,
+  ThemeProvider,
+  createTheme,
+  Button,
+  Link,
+} from "@mui/material";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  const theme = createTheme();
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }} />
+          <Typography component="h1" variant="h5">
+            Sign Up
+          </Typography>
+          <Box component="form" sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="fname"
+                  required
+                  fullWidth
+                  id="fname"
+                  label="First Name"
+                />
+              </Grid>
+
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="family-name"
+                  name="lname"
+                  required
+                  fullWidth
+                  id="lname"
+                  label="Last Name"
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="email"
+                  name="email"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  type="email"
+                />
+              </Grid>
+
+              <Grid item xs={12}>
+                <TextField
+                  name="email"
+                  required
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  type="password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              variant="contained"
+              fullWidth
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/" variant="body2">
+                  Already have an account ? Sign In
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default SignUp;
