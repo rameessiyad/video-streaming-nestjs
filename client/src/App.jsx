086c25b86ead66fa40c6769f5c7307c1369cab2d
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-18 16:13:48 +0530

    signin ui added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Routing from "./Routing";
 
 const App = () => {
-  const [isLoggedIn, setLoggedIn] = useState(true);
+  const [isLoggedIn, setLoggedIn] = useState(false);
   return <Routing isLoggedIn={isLoggedIn} setLoggedIn={setLoggedIn} />;
 };
 
--- client/src/components/Auth/SignIn.jsx
+++ client/src/components/Auth/SignIn.jsx
@@ -1,5 +1,80 @@
-const SignIn = () => {
-  return <div>SignIn</div>;
+/* eslint-disable no-unused-vars */
+
+import { ThemeProvider } from "@emotion/react";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  createTheme,
+  CssBaseline,
+  Grid2,
+  TextField,
+  Typography,
+  Link,
+} from "@mui/material";
+
+// eslint-disable-next-line react/prop-types
+const SignIn = ({ setLoggedIn }) => {
+  const theme = createTheme();
+  return (
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}></Avatar>
+          <Typography component="h1" variant="h5">
+            Sign In
+          </Typography>
+          <Box component="form">
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+            />
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign in
+            </Button>
+            <Grid2 container>
+              <Grid2 item xs>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid2>
+            </Grid2>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
 };
 
 export default SignIn;
