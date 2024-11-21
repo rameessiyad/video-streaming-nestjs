3829b44012282ef52f87dfb6914507b0558a0513
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-11-21 12:02:00 +0530

    vidoe component updated with api call

--- client/src/components/Video/Video.jsx
+++ client/src/components/Video/Video.jsx
@@ -1,6 +1,13 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { Container, Grid, CardActionArea } from "@mui/material";
+import {
+  Container,
+  Grid,
+  CardActionArea,
+  Card,
+  CardContent,
+  Typography,
+} from "@mui/material";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
@@ -37,7 +44,33 @@ const Video = ({ setLoggedIn }) => {
   return (
     <Container>
       <Grid item xs={12} md={12} marginTop={2}>
-        <CardActionArea component="a" href="#"></CardActionArea>
+        <CardActionArea component="a" href="#">
+          <Card sx={{ display: "flex" }}>
+            <CardContent sx={{ flex: 1 }}>
+              <video autoPlay controls width="200">
+                <source
+                  src={`http://localhost:5000/video/${videoId}`}
+                  type="video/mp4"
+                />
+              </video>
+            </CardContent>
+          </Card>
+        </CardActionArea>
+        <Grid container spacing={2} marginTop={2}>
+          <Grid item xs={12} md={6}>
+            <Typography variant="subtitle1" color="primary">
+              Created by:{videoInfo.createdBy?.fullname}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Typography variant="subtitle1" color="primary">
+              Created: {videoInfo.uploadDate}
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={12}>
+            <Typography variant="h5">{videoInfo.title}</Typography>
+          </Grid>
+        </Grid>
       </Grid>
     </Container>
   );
